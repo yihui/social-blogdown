31511313b5385098ea8416ebf1b57ce4fc2397e6
Yihui Xie <xie@yihui.name>
2023-12-26 18:58:32 -0600

    switch from twitter to bsky

--- R/fetch.R
+++ R/fetch.R
@@ -1,42 +1,64 @@
-options('rtweet:::config_dir' = normalizePath('~'))
 dir.create('content/post', showWarnings = FALSE)
 d = Sys.Date()
 
-update.packages(ask = FALSE, checkBuilt = TRUE)
-if (!requireNamespace('rtweet', quietly = TRUE)) install.packages('rtweet')
+if (!require('xfun')) install.packages('xfun')
+xfun::pkg_load2('jsonlite')
 
-if (!file.exists(f <- 'R/keywords.csv')) writeLines('query,since_id', f)
+update.packages(ask = FALSE, checkBuilt = TRUE)
+
+if (!file.exists(f <- 'R/keywords.csv')) writeLines('query,since,limit', f)
 m = read.csv(f, colClasses = c('character', 'character', 'integer'))
 d = as.character(d)
-x = NULL; t = paste('Tweets on', d); n = 0  # markdown text, post title, and favorite count
-ids = NULL  # ids of tweets that have already been included in the post
+x = NULL; t = paste('Posts retrieved on', d); n_like = 0  # markdown text, post title
+ids = NULL  # ids of posts that have already been included
+
+search_bsky = function(query, since = 0, limit = 0) {
+  extra = ''; fmt = '%Y-%m-%dT%H:%M:%OSZ'
+  since = if (since <= 0) as.Date(0) else strptime(since, fmt, tz = 'UTC') + 1  # offset by 1 sec
+  since = format(since, fmt)
+  if (limit <= 0) limit = 100 else {
+    # retrieve posts before the last 7 days
+    extra = paste0('&sort=top&&until=', format(Sys.time() - 7 * 24 * 3600, fmt))
+  }
+  u = sprintf(
+    'https://public.api.bsky.app/xrpc/app.bsky.feed.searchPosts?q=%s&since=%s&limit=%d%s',
+    query, since, limit, extra
+  )
+  x = xfun::read_utf8(u)
+  jsonlite::fromJSON(x)$posts
+}
 
 for (i in seq_len(NROW(m))) {
   q = m[i, 'query']
-  s = rtweet::search_tweets(q, n = 1000, include_rts = FALSE, since_id = m[i, 'since_id'])
-  if (NROW(s) == 0 || is.na(s$favorite_count[1])) next
-  u = rtweet::users_data(s)
-  k = !(s$id_str %in% ids) & (s$favorite_count + s$retweet_count >= m[i, 'threshold']) &
-    (unlist(lapply(s$entities, function(x) sum(!is.na(x$hashtags$text)))) < 10)
-  s = s[k, ]
-  u = u[k, ]
-  if (nrow(s) == 0) next
-  ids = c(ids, s$id_str)
-
-  m[i, 'since_id'] = s$id_str[1]  # update since_id for newer results next time
-  k = order(s$favorite_count, s$retweet_count, decreasing = TRUE)
-  s = s[k, ]
-  u = u[k, ]
-  if ((n2 <- s$favorite_count[1]) >= n) {
-    n = n2; t = gsub('\\s+', ' ', s$text[1])
+  s = search_bsky(q, m[i, 'since'], m[i, 'limit'])
+  if (NROW(s) == 0) next
+  s = s[!basename(s$uri) %in% ids, ]
+  if (NROW(s) == 0) next
+  s = s[order(s$likeCount, decreasing = TRUE), ]
+  id = basename(s$uri); ids = c(ids, id)
+  m[i, 'since'] = max(s$indexedAt)  # update 'since' for newer results next time
+  h = s$author$handle
+  b = s$record$text
+  b = gsub('```', '\\`\\`\\`', b, fixed = TRUE)
+  b = gsub('\\s*$', '', b)
+  # use most liked post as title
+  if (max(s$likeCount) > n_like) {
+    t = xfun::strip_html(gsub('\n', ' ', b[which.max(s$likeCount)]))
+    n_like = max(s$likeCount)
   }
-  s$text = gsub('\\s*$', '', s$text)
-  s$text = gsub('\n\n', '\n>\n', s$text)
-  s$text = paste(s$text, sprintf(' [&#8618;](https://twitter.com/%s/status/%s)', u$screen_name, s$id_str))
+  b = paste(b, sprintf(' [&#8618;](https://bsky.app/profile/%s/post/%s)', h, id))
+  links = unlist(lapply(s$record$facets, function(fc) {
+    u = unlist(lapply(fc$features, function(ft) {
+      ft[ft[, '$type'] == 'app.bsky.richtext.facet#link', ]$uri
+    }))
+    if (length(u)) paste0('\n\n', paste0('- <', u, '>', collapse = '\n')) else ''
+  }))
   x = c(
-    x, paste('#', gsub(' .+', '', q)), '',
-    paste0('> **', u$name, '** (@', u$screen_name, '; ', s$favorite_count, '/',
-           s$retweet_count, '): ', gsub('\n', '\n> ', s$text), '\n\n<!-- -->\n\n')
+    x, paste('#', gsub(' .+', '', q)), '', paste0(
+      '**', trimws(s$author$displayName), '** (@', h, '; ', s$likeCount, '/',
+      s$replyCount, '/', s$repostCount, '; ', as.Date(s$indexedAt),
+      '): ', b, links, collapse = '\n\n---\n\n'
+    ), ''
   )
   Sys.sleep(1)
 }
--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# twitter-blogdown
-
-Search Twitter messages through predefined keywords using [**rtweet**](https://github.com/ropensci/rtweet) Github Actions on a [weeky](https://github.com/yihui/twitter-blogdown/blob/master/.github/workflows/twitter-blogdown.yaml) basis, and convert the results to Markdown, and build a website using [**blogdown**](https://github.com/rstudio/blogdown).
+Search social media messages on a weekly basis, convert the results to Markdown, and build a website using [**blogdown**](https://github.com/rstudio/blogdown), so I can read social media only once a week instead of every minute.
